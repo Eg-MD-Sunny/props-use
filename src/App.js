588c042c9b3e18c046f69f8219357bc363324c31
Naim Zaman <egnaimur@gmail.com>
2021-05-26 08:43:29 +0600

    Dynamic Data Pass Using Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		  <p>I am a React Person</p>
+
+		  <Person name="Munna" job="Football"></Person>   {/*<<======React Component Call*/}
+		  <Person name="Masum" job="Dorshok"></Person>   {/*<<======React Component Call*/}
       </header>
     </div>
   );
+}
+
+//======>>Component Function Create
+function Person(dataStore){
+	let personStyle = {
+		border:'1px solid white',
+		padding:'20px',
+		marginBottom:'15px',
+		borderRadius:'7px'
+	}
+
+	return (
+		// <div style={{border:'1px solid white'}}>
+		<div style={personStyle}>
+			<h3>My Name : {dataStore.name}</h3>
+			<p>My Profession : {dataStore.job}</p>
+		</div>
+	)
 }
 
 export default App;
